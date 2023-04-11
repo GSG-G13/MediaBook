75ab6b874017b8dd37ae40a5d15bc921f0962f6e
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-04-11 12:57:10 +0300

    validation Login

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const router = require('./routes/index');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "..", "public")));
-
+app.use(router);
 module.exports = app;
