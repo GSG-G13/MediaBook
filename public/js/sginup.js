aa3054aa4ac9a011fc5188913b3dc15279c04d00
alaaahmade <alaaahmade66@gmail.com>
2023-04-11 12:52:55 +0200

    Create Validation And fetch from clint side

--- public/js/sginup.js
+++ public/js/sginup.js
@@ -1,14 +1,42 @@
-import * as yup from 'yup';
 
 const form = document.getElementById('signupForm');
 const firstnameInput = document.getElementById('firstnameInput')
 const surnameInput = document.getElementById('surnameInput');
 const emailInput = document.getElementById('emailInput');
 const passwordInput = document.getElementById('passwordInput');
-const email = document.getElementById('email');
 const dateInput = document.getElementById('dateInput0');
 const gender = document.getElementById('gender');
+const signInBtn  = document.getElementById('signUpSubmit');
+
+signInBtn.addEventListener('click', () =>{
+    if(firstnameInput.value.length < 3 || firstnameInput.value.length > 40){
+        window.alert('First name must be at least 3 characters and less than 40 characters long ')
+        return;
+    }else if(surnameInput.value.length < 3 ||surnameInput.value.length > 40){
+        window.alert('surname must be at least 3 characters and less than 40 characters long ')
+        return;
+    }else if(emailInput.value.length < 3 ||emailInput.value.length > 80 ){
+        window.alert('email must be at least 3 characters and less than 40 characters long ')
+        return;
+    }else if(passwordInput.value.length < 8 ||passwordInput.value.length > 80 ){
+        window.alert('password must be at least 8 characters and less than 40 characters long ')
+        return;
+    }
+    const body = {
+        firstname: firstnameInput.value,
+        surname: surnameInput.value,
+        email: emailInput.value,
+        password: passwordInput.value,
+        date: dateInput.value,
+        gender: gender.value,
+    }
+
+    fetch('/add/user', {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(body),
+    }).then(console.log)
+    
+})
 
 
-
-const signUpSchem = yup.
