4372482f3592e175d665d29d4e278cc6e16e67ab
alaaahmade <alaaahmade66@gmail.com>
2023-04-11 12:07:51 +0200

    l

---
+++ public/js/sginup.js
@@ -0,0 +1,14 @@
+import * as yup from 'yup';
+
+const form = document.getElementById('signupForm');
+const firstnameInput = document.getElementById('firstnameInput')
+const surnameInput = document.getElementById('surnameInput');
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
+const email = document.getElementById('email');
+const dateInput = document.getElementById('dateInput0');
+const gender = document.getElementById('gender');
+
+
+
+const signUpSchem = yup.
