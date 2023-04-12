7729d402eda747708a9fbef9fddcb6e5fc401258
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-04-12 20:09:18 +0300

    create post posts query relates #40

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,6 @@
+const postPostsQuery = require("./queries/postPostsQuery")
 const getPostsQuery = require("./queries/getPostsQuery");
 const signupQuery = require("./queries/signupQuery");
 const Build = require("./config/build");
 
-module.exports = { signupQuery, Build, getPostsQuery };
+module.exports = { signupQuery, Build, getPostsQuery,postPostsQuery };
---
+++ server/database/queries/postPostsQuery.js
@@ -0,0 +1,9 @@
+const connection = require("../connection");
+const postPostsQuery = ({content,image_url})=>{
+    const options = {
+        text:`insert into posts (content , image_url) values ($1 , $2) returning content , image_url`,
+        values:[content,image_url]
+    };
+    return connection.query(options)
+};
+module.exports = postPostsQuery;
