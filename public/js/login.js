dd04d8e99b62709868972e655fe8868446f8f189
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-04-12 11:26:30 +0300

    validate frontend login client relates #30

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,20 @@ btn.addEventListener("click", (e) => {
   e.preventDefault();
   const email = document.getElementById("email");
   const password = document.getElementById("pass");
-
+  if (email.value.length < 3 || email.value.length > 80) {
+    window.alert(
+      "email must be at least 3 characters and less than 40 characters long "
+    );
+    return;
+  } else if (
+    password.value.length < 8 ||
+    password.value.length > 80
+  ) {
+    window.alert(
+      "password must be at least 8 characters and less than 40 characters long "
+    );
+    return;
+  }
   fetch("/login", {
     method: "POST",
     headers: {
@@ -16,9 +29,17 @@ btn.addEventListener("click", (e) => {
     }),
   })
     .then((data) => data.json())
-    .catch((err) => {
-      errores.forEach(element => {
-        
-      });
-    });
+    .then((data) => {
+      const divmessage = document.getElementById("divmessage")
+      if(data.errors == true){
+        const parmessage = document.createElement('p');
+        parmessage.textContent = data.message;
+        divmessage.appendChild(parmessage);
+      }else{
+        window.alert('Login Successfully, Go TO Profile')
+      }
+    })
+    .catch(err=> console.log(err));
+    const FormData = document.getElementById("form_data")
+    FormData.reset();
 });
